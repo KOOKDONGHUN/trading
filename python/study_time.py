b059798cd6841832cbe84d6ef1d756c80220b766
KOOKDONGHUN <dh3978@naver.com>
2020-06-29 16:30:08 +0900

    test

---
+++ python/study_time.py
@@ -0,0 +1,14 @@
+import time
+
+print(time.time()) # 1593415583.5009496
+
+print(time.ctime()) # Mon Jun 29 16:26:23 2020
+
+print(dir(time))
+'''['_STRUCT_TM_ITEMS', '__doc__', '__loader__', '__name__', '__package__',
+ '__spec__', 'altzone', 'asctime', 'clock', 'ctime', 'daylight', 'get_clock_info',
+  'gmtime', 'localtime', 'mktime', 'monotonic', 'monotonic_ns', 'perf_counter', 'perf_counter_ns', 
+  'process_time', 'process_time_ns', 'sleep', 'strftime', 'strptime', 'struct_time', 'thread_time', 
+  'thread_time_ns', 'time', 'time_ns', 'timezone', 'tzname']'''
+
+  
